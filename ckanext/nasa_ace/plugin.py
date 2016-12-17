4672fe4b90c41fb2517bbf63a229944d70b0d04d
Bob Torgerson <rltorgerson@alaska.edu>
2016-12-16 17:01:17 -0900

    New additions to plugin.py for CometChat integration.

--- ckanext/nasa_ace/plugin.py
+++ ckanext/nasa_ace/plugin.py
@@ -5,6 +5,7 @@ import actions
 class Nasa_AcePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IAuthenticator)
 
     # IConfigurer
     def update_config(self, config_):
@@ -14,5 +15,23 @@ class Nasa_AcePlugin(plugins.SingletonPlugin):
 
     # IActions
     def get_actions(self):
-        return {'user_show': actions.user_show, 
-                'user_create': actions.user_create }
+        return {'user_show': actions.user_show,
+                'user_create': actions.user_create,
+                'user_update': actions.user_update,
+                'user_delete': actions.user_delete }
+
+    # IAuthenticator
+    # This requires that I define identify and login even if I don't use them.
+    def identify(self):
+        pass
+
+    def login(self):
+        pass
+
+    def abort(self):
+        pass
+
+    # Set CC_DATA to 0 to logout the current user in CometChat.
+    def logout(self):
+        toolkit.response.set_cookie('cc_data', '0')
+
