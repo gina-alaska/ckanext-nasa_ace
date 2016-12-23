a4e7a0b117981973524fcfc4473bdb21a5b0ab0e
Bob Torgerson <rltorgerson@alaska.edu>
2016-12-23 11:46:36 -0900

    Modified NASA theme to update the config file with the MySQL configuration.

---
+++ ckanext/nasa_ace/config.py
@@ -0,0 +1,4 @@
+def chat_connect():
+    # This must be modified to connect to your MySQL database similar to:
+    # "mysql://<mysql_user>:<mysql_user_password>@<db_host>/<db_table_name>?use_unicode=0"
+    return "mysql://connection_information_here"
