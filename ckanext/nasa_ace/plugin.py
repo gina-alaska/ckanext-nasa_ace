0215e3daa9fa7eaf0690658470adaa1423ff3792
rwspicer <rwspicer@alaska.edu>
2017-03-09 13:42:46 -0900

    redirects '/' to '/datasets'

--- ckanext/nasa_ace/plugin.py
+++ ckanext/nasa_ace/plugin.py
@@ -4,10 +4,16 @@ import ckan.plugins.toolkit as toolkit
 
 class Nasa_AcePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
+    plugins.implements(plugins.IRoutes, inherit=True)
+   
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'nasa_ace')
+        
+    #IRoutes
+    def before_map(self, m):
+        """redirect index to dataset """
+        m.redirect('/', '/dataset')
+        return m
